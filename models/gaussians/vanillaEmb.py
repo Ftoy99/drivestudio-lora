5b07430b92cd657521042838895cbc4cbc34c147
Ftoy <spit10000@yahoo.gr>
2025-02-01 12:25:15 +0200

    Device debug

--- models/gaussians/vanillaEmb.py
+++ models/gaussians/vanillaEmb.py
@@ -381,20 +381,18 @@ class VanillaGaussiansEmb(nn.Module):
 
         self._features_dc = Parameter(self._features_dc[~culls].detach())
         self._features_rest = Parameter(self._features_rest[~culls].detach())
-        print(self._features_rest.detach().device)
+
 
         means = self._means.weight[~culls].detach()
         scales = self._scales.weight[~culls].detach()
         quats = self._quats.weight[~culls].detach()
         opacities = self._opacities.weight[~culls].detach()
 
-        print(means.device)
         self._means = nn.Embedding(means.size(0), self._means.weight.size(1))
         self._scales = nn.Embedding(scales.size(0), self._scales.weight.size(1))
         self._quats = nn.Embedding(quats.size(0), self._quats.weight.size(1))
         self._opacities = nn.Embedding(opacities.size(0), self._opacities.weight.size(1))
         # self.colors_all = nn.Embedding(self.colors_all.weight[~culls].size(0), self.colors_all.size(1))
-        print(self._means.weight.device)
 
         self._means.weight.data.copy_(means)
         self._scales.weight.data.copy_(scales)
