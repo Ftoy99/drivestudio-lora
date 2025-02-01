616dc639fa126171083dcb14b4facc3aa535d868
Ftoy <spit10000@yahoo.gr>
2025-02-01 17:47:17 +0200

    Debug optim parameters

--- models/gaussians/basics.py
+++ models/gaussians/basics.py
@@ -209,6 +209,11 @@ def dup_in_optim(optimizer, dup_mask, param_dict, n=2):
                 print(f"Warning: 'exp_avg' missing for {name}, initializing.")
                 param_state["exp_avg"] = torch.zeros_like(old_params)
 
+            # Check if 'exp_avg' exists, if not, initialize it
+            if "exp_avg_sq" not in param_state:
+                print(f"Warning: 'exp_avg' missing for {name}, initializing.")
+                param_state["exp_avg"] = torch.zeros_like(old_params)
+
             repeat_dims = (n,) + tuple(1 for _ in range(param_state["exp_avg"].dim() - 1))
             param_state["exp_avg"] = torch.cat(
                 [param_state["exp_avg"],
