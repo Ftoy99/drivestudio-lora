31c2981123673636c09312ec069355e2fa84dbf0
Ftoy <spit10000@yahoo.gr>
2025-02-02 11:45:54 +0200

    Train lora

--- tools/train_lora.py
+++ tools/train_lora.py
@@ -282,21 +282,6 @@ def main(args):
         device=device
     )
 
-    # Add the lora peft
-    from peft import LoraConfig, TaskType, get_peft_model
-
-    lora_config = LoraConfig(
-        r=8,
-        lora_alpha=8,
-        init_lora_weights="gaussian",
-        target_modules=["deform_network.linear.0", "deform_network.linear.1", "deform_network.linear.2",
-                        "deform_network.linear.3", "deform_network.linear.4", "deform_network.linear.5",
-                        "deform_network.linear.6", "deform_network.linear.7", ],
-        task_type=TaskType.FEATURE_EXTRACTION
-    )
-    # Apply LoRA
-    lora_model = get_peft_model(trainer.models["DeformableNodes"], lora_config)
-
     # NOTE: If resume, gaussians will be loaded from checkpoint
     #       If not, gaussians will be initialized from dataset
     if args.resume_from is not None:
@@ -312,6 +297,21 @@ def main(args):
         logger.info(
             f"Training from scratch, initializing gaussians from dataset, starting at step {trainer.step}"
         )
+
+    # Add the lora peft
+    from peft import LoraConfig, TaskType, get_peft_model
+
+    lora_config = LoraConfig(
+        r=8,
+        lora_alpha=8,
+        init_lora_weights="gaussian",
+        target_modules=["deform_network.linear.0", "deform_network.linear.1", "deform_network.linear.2",
+                        "deform_network.linear.3", "deform_network.linear.4", "deform_network.linear.5",
+                        "deform_network.linear.6", "deform_network.linear.7", ],
+        task_type=TaskType.FEATURE_EXTRACTION
+    )
+    # Apply LoRA
+    lora_model = get_peft_model(trainer.models["DeformableNodes"], lora_config)
 
     if args.enable_viewer:
         # a simple viewer for background visualization
